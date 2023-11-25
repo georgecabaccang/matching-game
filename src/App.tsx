1549747f26a003605593b878c32a542439cb4782
georgesycabaccang <georgesycabaccang@gmail.com>
2023-11-25 19:01:10 +0800

    Split high scores into components. Make getting of scores from DB work.

--- src/App.tsx
+++ src/App.tsx
@@ -3,59 +3,7 @@ import { useContext, useEffect } from "react";
 import { TileContext } from "./store/tile-context/TileContext";
 import Timer from "./components/timer/Timer";
 import { TimerContext } from "./store/timer-context/TimerContext";
-
-const DUMMY_LEADERS = [
-    {
-        id: 1,
-        name: "Test Name Syempre",
-        time: "00:00:00",
-    },
-    {
-        id: 2,
-        name: "Test Name Syempre",
-        time: "00:00:00",
-    },
-    {
-        id: 3,
-        name: "Test Name Syempre",
-        time: "00:00:00",
-    },
-    {
-        id: 4,
-        name: "Test Name Syempre",
-        time: "00:00:00",
-    },
-    {
-        id: 5,
-        name: "Test Name Syempre",
-        time: "00:00:00",
-    },
-    {
-        id: 6,
-        name: "Test Name Syempre",
-        time: "00:00:00",
-    },
-    {
-        id: 7,
-        name: "Test Name Syempre",
-        time: "00:00:00",
-    },
-    {
-        id: 8,
-        name: "Test Name Syempre",
-        time: "00:00:00",
-    },
-    {
-        id: 9,
-        name: "Test Name Syempre",
-        time: "00:00:00",
-    },
-    {
-        id: 10,
-        name: "Test Name Syempre",
-        time: "00:00:00",
-    },
-];
+import HighScores from "./components/scores/HighScores";
 
 function App() {
     const tileContext = useContext(TileContext);
@@ -82,32 +30,7 @@ function App() {
                     </div>
                 )}
                 {/* if game is over, display */}
-                {timerContext.isGameOver && (
-                    <div className="w-[60%] h-[100%] flex justify-center flex-wrap">
-                        <ul className="h-[50%] w-[100%] justify-between items-center flex flex-wrap flex-col">
-                            {DUMMY_LEADERS.map((leader, index) => {
-                                return (
-                                    <li
-                                        className="h-[20%] w-[50%] flex justify-center items-center"
-                                        key={leader.id}
-                                    >
-                                        <div className="flex gap-3 w-[100%] lg:px-8 xl:px-14 relative">
-                                            <div
-                                                className={`absolute ${
-                                                    index === 9 ? "-ml-[0.3rem]" : ""
-                                                }`}
-                                            >{`${index + 1}`}</div>
-                                            <div className="flex justify-between w-[100%] items-center ml-5">
-                                                <div className="">{leader.name}</div>
-                                                <div>{leader.time}</div>
-                                            </div>
-                                        </div>
-                                    </li>
-                                );
-                            })}
-                        </ul>
-                    </div>
-                )}
+                {timerContext.isGameOver && <HighScores />}
             </div>
         </>
     );
