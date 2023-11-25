15935bc796c79c736c957961f70f87089c080914
georgesycabaccang <georgesycabaccang@gmail.com>
2023-11-25 22:38:33 +0800

    Create new document through client side. Still use hard coded 'mode' and 'size'

--- src/components/forms/AddScoreForm.tsx
+++ src/components/forms/AddScoreForm.tsx
@@ -1,6 +1,25 @@
+import { useContext, useRef } from "react";
+import { TimerContext } from "../../store/timer-context/TimerContext";
+import { addScore } from "../../api-requests/scoreRequests";
+
 export default function AddScoreForm() {
+    const timerContext = useContext(TimerContext);
+    const nameRef = useRef<HTMLInputElement>(null);
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        const payload = {
+            name: nameRef.current!.value,
+            time: timerContext.totalTimeInMilliseconds,
+            mode: "numbers",
+            size: 24,
+        };
+        await addScore(payload);
+    };
+
     return (
-        <div className="w-[50%] flex items-center flex-col">
+        <form onSubmit={handleSubmit} className="w-[50%] flex items-center flex-col">
             <span className="text-[1.1rem] font-bold">Congratulations!</span>
             <span className="text-blue-400"> You made the Top 10!</span>
             <label htmlFor="enter-name" className="text-[0.8rem] font-semibold">
@@ -10,10 +29,11 @@ export default function AddScoreForm() {
                 id="enter-name"
                 type="text"
                 autoFocus
+                ref={nameRef}
                 maxLength={20}
                 className="px-[4rem] overflow-x-visible bg-transparent border-b-2 w-[100%] text-center border-blue-200 focus:outline-none py-2"
             />
             <span className="text-[0.6rem] mt-2">Max of 20 Characters</span>
-        </div>
+        </form>
     );
 }
