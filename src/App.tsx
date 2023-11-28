c2b0723303d9cf96b25b0a25b20492925d23a2ae
georgesycabaccang <georgesycabaccang@gmail.com>
2023-11-28 18:50:39 +0800

    Fix responsiveness of reset button. Also add png for md below breakpoints.

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { TimerContext } from "./store/timer-context/TimerContext";
 import HighScores from "./components/scores/HighScores";
 import FullScreenModal from "./components/utils/modals/FullScreenModal";
 import { GameContext } from "./store/game-context/GameContext";
+import resetIcon from "./assets/icons/reset-icon.png";
 
 function App() {
     const tileContext = useContext(TileContext);
@@ -71,15 +72,28 @@ function App() {
                 )}
             </div>
             {gameContext.isGameSet && (
-                <div className="w-auto h-auto absolute top-10 right-10">
-                    <button
-                        type="button"
-                        className="bg-blue-300 rounded-md px-3 py-1 hover:bg-blue-400 hover:scale-95 shadow-md hover:shadow-none transition duration-75 font-semibold hover:font-normal"
-                        onClick={restartGame}
-                    >
-                        Restart Game
-                    </button>
-                </div>
+                <>
+                    <div className={`w-auto h-auto absolute top-10 right-10 md:block hidden`}>
+                        <div
+                            className="bg-blue-300 text-[1rem] rounded-md px-3 py-1 flex hover:bg-blue-400 hover:scale-95 shadow-md hover:shadow-none transition duration-75 font-semibold hover:font-normal"
+                            onClick={restartGame}
+                        >
+                            <div className="w-[1.5rem] mt-[0.1rem] me-[0.2rem]">
+                                <img src={resetIcon} alt="reset-svg" />
+                            </div>
+                            <div>Restart Game</div>
+                        </div>
+                    </div>
+                    <div className={`w-[2.5rem] absolute top-5 right-5 block md:hidden`}>
+                        <button
+                            type="button"
+                            className="bg-blue-300 rounded-md px-1 py-1 hover:bg-blue-400 hover:scale-95 shadow-md hover:shadow-none transition duration-75 font-semibold hover:font-normal"
+                            onClick={restartGame}
+                        >
+                            <img src={resetIcon} alt="reset-svg" />
+                        </button>
+                    </div>
+                </>
             )}
         </>
     );
