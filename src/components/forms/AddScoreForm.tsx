804934b00f92335a7938853ffe59609afbb99051
georgesycabaccang <georgesycabaccang@gmail.com>
2023-11-30 16:56:17 +0800

    Add breakpoints for AddForm texts

--- src/components/forms/AddScoreForm.tsx
+++ src/components/forms/AddScoreForm.tsx
@@ -30,10 +30,13 @@ export default function AddScoreForm({
     };
 
     return (
-        <form onSubmit={handleSubmit} className="w-[50%] flex items-center flex-col">
-            <span className="text-[1.1rem] font-bold">Congratulations!</span>
-            <span className="text-blue-400"> You made the Top 10!</span>
-            <label htmlFor="enter-name" className="text-[0.8rem] font-semibold">
+        <form
+            onSubmit={handleSubmit}
+            className="w-[100%] flex items-center flex-col px-[3rem] xs:px-[5rem] xs:text-[1.1rem] sm:px-[12rem] sm:text-[1.2rem] md:px-[15rem] md:text-[1.3rem] lg:px-[23rem] xl:px-[30rem] xl:text-[1.5rem] xxl:px-[35rem] xxl:text-[1.7rem] xxxl:px-[45rem] xxxl:text-[2.2rem] xxxxl:px-[60rem] xxxxl:text-[3.2rem]"
+        >
+            <span className="text-[1.1em] font-bold">Congratulations!</span>
+            <span className="text-blue-400 text-[1em]"> You made the Top 10!</span>
+            <label htmlFor="enter-name" className="text-[0.8em] font-semibold">
                 Enter Desired Name:
             </label>
             <input
@@ -42,9 +45,9 @@ export default function AddScoreForm({
                 autoFocus
                 ref={nameRef}
                 maxLength={10}
-                className="px-[4rem] overflow-x-visible bg-transparent border-b-2 w-[100%] text-center border-blue-200 focus:outline-none py-2"
+                className="px-[4rem] text-[0.8em] overflow-x-visible bg-transparent border-b-2 w-[100%] text-center border-blue-200 focus:outline-none py-2"
             />
-            <span className="text-[0.6rem] mt-2">Max of 10 Characters</span>
+            <span className="text-[0.5em] mt-2">Max of 10 Characters</span>
         </form>
     );
 }
--- src/components/scores/HighScores.tsx
+++ src/components/scores/HighScores.tsx
@@ -111,7 +111,7 @@ export default function HighScores({
     }, []);
     return (
         <div className="w-[100%] h-[100%] md:justify-normal flex justify-center flex-wrap">
-            <ul className="h-[50%] w-[100%] overflow-y-scroll md:px-[5rem] md:flex-col md:w-[100%] md:overflow-x-hidden justify-between items-center flex flex-wrap flex-row px-[2rem]">
+            <ul className="h-[50%] w-[100%] overflow-y-scroll sm:px-[7rem] md:px-[5rem] md:flex-col md:w-[100%] md:overflow-x-hidden justify-between items-center flex flex-wrap flex-row px-[2rem]">
                 {highScores &&
                     highScores.map((leader, index) => {
                         return (
