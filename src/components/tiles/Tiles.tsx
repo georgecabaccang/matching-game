c7495df708a0a3f57f2c483665c50a4595ee89d5
georgesycabaccang <georgesycabaccang@gmail.com>
2023-12-01 23:04:28 +0800

    Take out resetMatches from setTimeout in resetTile of Tile.tsx for faster resetting of matches.

--- src/components/tiles/Tiles.tsx
+++ src/components/tiles/Tiles.tsx
@@ -52,8 +52,8 @@ export default function Tiles() {
     const resetTile = async () => {
         count.current = 0;
         setDisabled(true);
+        resetMatches();
         setTimeout(() => {
-            resetMatches();
             setHasTwoShownTiles(true);
             setDisabled(false);
         }, 100);
